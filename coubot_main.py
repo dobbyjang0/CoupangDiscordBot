749f6e38d459df92dbe4654fa1eb50eba0f6d886
Hostagen <68284806+Hostagen@users.noreply.github.com>
2021-03-15 01:06:06 +0900

    알고리즘 개선 및 기능 개선

--- coubot_main.py
+++ coubot_main.py
@@ -1,57 +1,87 @@
 import nest_asyncio
 import asyncio
 import discord
-from discord.ext import commands, tasks
 import pandas
+import os
+from discord.ext import commands, tasks
+from dotenv import load_dotenv
 
 nest_asyncio.apply()
 
 bot = commands.Bot(command_prefix="!")
+load_dotenv("token.env")
 
 @bot.event
 async def on_ready():
     print("--- 연결 성공 ---")
-    print(f"봇 이름: {bot.user.name}")
+    print(f"봇 이름: {bot.user}")
     print(f"ID: {bot.user.id}")
-    return
 
-@bot.command()
-async def 쿠팡(ctx, service_type=None):
-    if service_type in [None, "메인", "기본", "홈"]:
-        embed = discord.Embed(title= "쿠팡", url="https://coupa.ng/bSQJi8")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/804815694717911080/817096183637344286/img.png")
-        
-        description_list=[]
-        description_list.append("쿠팡 기본 홈페이지입니다.")
-        description_list.append("▶ [**골드박스**](https://coupa.ng/bSQUxy)")
-        description_list.append("▶ [**로켓프레쉬**](https://coupa.ng/bSQUDh)")
-        description_list.append("▶ [**로켓와우**](https://coupa.ng/bSQUFP)")
-        description_list.append("▶ [**로켓직구**](https://coupa.ng/bSQUJ4)")
-        description_list.append("▶ [**로켓배송**](https://coupa.ng/bSQUMW)")
-        embed.description='\n'.join(description_list)
-        
-        await ctx.send(embed=embed)
-        return
-    elif service_type == "지원금":
-        # 지원금이 없는 경우 초기 지원금을 준다
-        # 24시간이 지나면 지원금을 준다
-        return
+@bot.group(name="쿠팡")
+async def coupang(ctx):
+    pass
 
-@bot.command()
-async def 킬(ctx):
-    if ctx.author.id not in [378887088524886016, 430377165629161482]:
-        await ctx.send("권한없음")
-        return
-    await ctx.send("봇 꺼짐")
-    await bot.close()    
+@coupang.command(name="메인", aliases=["기본", "홈"])
+async def Gcoupang_main(ctx):
+    descriptions = {
+        "골드박스": "https://coupa.ng/bSQUxy",
+        "로켓프레쉬": "https://coupa.ng/bSQUDh",
+        "로켓와우": "https://coupa.ng/bSQUFP",
+        "로켓직구": "https://coupa.ng/bSQUJ4",
+        "로켓배송": "https://coupa.ng/bSQUMW"
+    }
+    embed = discord.Embed(title="쿠팡", description="\n".join("▶ [**%s**](<%s>)" % (k, v) for k, v in descriptions.items()), url="https://coupa.ng/bSQJi8")
+    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/804815694717911080/817096183637344286/img.png")
+    await ctx.send(embed=embed)
 
-def main():
-    if __name__ == "__main__":
-        #봇 실행
-        with open("bot_token.txt", mode='r', encoding='utf-8') as txt:
-            bot_token = txt.read()
-        bot.run(bot_token)
-        
+@coupang.command(name="지원금")
+async def fund(ctx):
+    #do stuff
+    pass
 
-        
-main()
+async def get_appinfo():
+    return await bot.application_info()
+
+def is_teamsmembers():
+    def predicate(ctx):
+        app_info = asyncio.run(get_appinfo())
+        team = app_info.team
+        return ctx.author in team.members
+    return commands.check(predicate)
+
+@bot.command(name="킬")
+@is_teamsmembers()
+async def kill_bot(ctx):
+    global timer
+    timer = 5
+    embed = discord.Embed(title="봇이 %d 초 후 종료됩니다." % timer)
+    msg = await ctx.send(embed=embed)
+
+    async def countdown():
+        global timer
+        await msg.add_reaction("❌")
+
+        while timer:
+            timer -= 1
+            await asyncio.sleep(1.0)
+            embed.title = "봇이 %d 초 후 종료됩니다." % timer
+            await msg.edit(embed=embed)
+        return False
+
+    done, pending = await asyncio.wait([
+        countdown(),
+        bot.wait_for('reaction_add', check=lambda r, u: r.emoji == "❌" and u == ctx.author)
+    ], return_when=asyncio.FIRST_COMPLETED)
+
+    for future in pending:
+        future.cancel()
+                
+    if not done.pop().result():
+        await msg.delete()
+        await bot.close()
+    else:
+        embed.title = "종료를 취소합니다."
+        await msg.edit(embed=embed, delete_after=5)
+
+if __name__ == "__main__":
+    bot.run(os.getenv('DISCORD_TOKEN'))
