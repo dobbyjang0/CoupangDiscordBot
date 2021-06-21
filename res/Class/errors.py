efe9b8e2a013d7ab1e88261829f6d42f4c4256a6
Hostagen <68284806+Hostagen@users.noreply.github.com>
2021-06-21 22:55:47 +0900

    add error `InstanceHasNotSession`.

--- res/Class/errors.py
+++ res/Class/errors.py
@@ -9,3 +9,9 @@ class NoneFormname(Exception):
 
     def __str__(self):
         return "%s는 존재하지 않는 Form명입니다." % self.form_name.title() + " %s" % self.message if self.msg else ""
+
+
+class InstanceHasNotSession(Exception):
+
+    def __init__(self, msg):
+        super().__init__(msg)
