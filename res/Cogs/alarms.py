898ee378a89de5ed6fb66ffd5d103621c1538b8e
Hostagen <68284806+Hostagen@users.noreply.github.com>
2021-06-24 17:02:47 +0900

    minor format.

--- res/Cogs/alarms.py
+++ res/Cogs/alarms.py
@@ -1,14 +1,13 @@
-import discord
 from discord.ext import commands
 
 from ..Class import db
 from ..Class import parser
 
+
 class AlarmCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
-    
+
     async def add_alarm(self, ctx, product_id, product_price=None):
         if product_id is None:
             await ctx.send(f'입력 오류')
@@ -30,16 +29,16 @@ class AlarmCog(commands.Cog):
         if not product_price:
             product_price = now_price
         
-        #알람 목록에 추가시킨다.
+        # 알람 목록에 추가시킨다.
         alarm_table = db.PriceAlarmTable()
-        alarm_table.insert(ctx.guild.id, ctx.channel.id, ctx.author.id, product_id, product_price)
+        await alarm_table.insert(ctx.guild.id, ctx.channel.id, ctx.author.id, product_id, product_price)
         
         await ctx.send((product_id, product_price, ctx.author.id, "저장완료"))
         
-        #스캔 목록에 추가시킨다.
+        # 스캔 목록에 추가시킨다.
         scan_table = db.ScanTable()
         if scan_table.read_by_id(product_id) is None:
-            scan_table.insert(product_id, now_price)
+            await scan_table.insert(product_id, now_price)
 
         print("스캔목록 저장완료")
         return
@@ -62,7 +61,7 @@ class AlarmCog(commands.Cog):
         product_id = int(product_id)
         
         alarm_table = db.PriceAlarmTable()
-        alarm_table.delete_by_id(ctx.author.id, product_id)
+        await alarm_table.delete_by_id(ctx.author.id, product_id)
         await ctx.send(f'{product_id}삭제 완료')
         return
     
@@ -79,9 +78,8 @@ class AlarmCog(commands.Cog):
         product_price = int(product_price)
         
         alarm_table = db.PriceAlarmTable()
-        alarm_table.update_price(ctx.channel.id, ctx.channel.id, ctx.author.id, product_id)
-        
-        
+        await alarm_table.update_price(ctx.channel.id, ctx.channel.id, ctx.author.id, product_id)
+
         await ctx.send(f'{product_id} {product_price}로 수정 완료')
         return
             
