d126bbbdd0e97c60018cfd365d90ce1a4dedde26
Hostagen <68284806+Hostagen@users.noreply.github.com>
2021-07-02 23:15:27 +0900

    label maker is moved to utils.py

--- coubot/utils.py
+++ coubot/utils.py
@@ -1,3 +1,4 @@
+from typing import List
 from discord_slash.model import ButtonStyle
 from discord_slash.utils.manage_components import create_button, create_actionrow
 
@@ -29,3 +30,34 @@ def is_startswith_http_url(url: str, allow_https: bool = True) -> bool:
         return url.startswith("https://")
 
     return False
+
+
+def label_maker(string: str):
+    split_strings: List[str] = string.split()
+    list_length = len(split_strings)
+
+    result = []
+    length = 0
+
+    for index, split_string in enumerate(split_strings, start=1):
+        len_string = len(split_string)
+
+        if list_length == index:
+            result.append(split_string[:25])
+            return " ".join(result)
+
+        if length >= 25:
+            return " ".join(result)
+
+        next_element = split_strings[index]
+
+        if 25 - length - len_string > len(next_element):
+            del split_strings[index]
+            list_length -= 1
+            text = f"{split_string} {next_element}"
+            length += len(text) + 2
+            result.append(text)
+
+            continue
+
+        return " ".join(result)
--- coubot_main.py
+++ coubot_main.py
@@ -5,7 +5,6 @@ import discord
 import asyncio
 import nest_asyncio
 
-from typing import List
 from tendo import singleton
 from dotenv import load_dotenv
 from discord.ext import commands
@@ -164,40 +163,9 @@ async def group_coupang_cmd_search(
     else:
         select_options = []
 
-        def label_maker(string: str):
-            split_strings: List[str] = string.split()
-            list_length = len(split_strings)
-
-            result = []
-            length = 0
-
-            for index, split_string in enumerate(split_strings, start=1):
-                len_string = len(split_string)
-
-                if list_length == index:
-                    result.append(split_string[:25])
-                    return " ".join(result)
-
-                if length >= 25:
-                    return " ".join(result)
-
-                next_element = split_strings[index]
-
-                if 25 - length - len_string > len(next_element):
-
-                    del split_strings[index]
-                    list_length -= 1
-                    text = f"{split_string} {next_element}"
-                    length += len(text) + 2
-                    result.append(text)
-
-                    continue
-
-                return " ".join(result)
-
         for idx, embed in enumerate(embeds):
             option = create_select_option(
-                label=label_maker(embed.title),
+                label=coubot.utils.label_maker(embed.title),
                 description=embed.description,
                 value=str(idx)
             )
