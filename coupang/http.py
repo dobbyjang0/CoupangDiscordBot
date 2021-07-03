648f4f197c0788c91e9dbb52077006a24b6d3eb9
Hostagen <68284806+Hostagen@users.noreply.github.com>
2021-07-04 04:41:45 +0900

    add headers.

--- coupang/http.py
+++ coupang/http.py
@@ -1,9 +1,47 @@
+import os
 import json
+import time
+import hmac
 import aiohttp
+import hashlib
 
 from .errors import Forbidden
 from urllib.parse import quote as _uriquote
 from typing import Optional, Any, Iterable, Dict
+
+
+def _dt():
+    os.environ['TZ'] = 'GMT+0'
+    return time.strftime('%y%m%d') + 'T'+time.strftime('%H%M%S') + 'Z'
+
+
+def auth(
+        method: str,
+        access_key: str,
+        secret_key: str,
+        path: str,
+        query: str = None
+):
+
+    dt = _dt()
+
+    if query is not None:
+        message = f"{dt}{method}{path}{query}"
+
+    else:
+        message = f"{dt}{method}{path}"
+
+    signature = hmac.new(
+        bytes(secret_key, "utf-8"),
+        message.encode("utf-8"),
+        hashlib.sha256
+    ).hexdigest()
+
+    authorization = f"""
+    CEA algorithm=HmacSHA256, access-key={access_key}, signed-date={dt}, signature={signature}
+    """
+
+    return authorization.strip()
 
 
 async def json_or_text(response: aiohttp.ClientResponse) -> Any:
@@ -35,7 +73,15 @@ class Route:
 
 class CoupangHTTPClient:
 
-    def __init__(self, connector: Optional[aiohttp.BaseConnector] = None):
+    def __init__(
+            self,
+            access_key: str,
+            secret_key: str,
+            connector: Optional[aiohttp.BaseConnector] = None
+    ):
+
+        self.access_key = access_key
+        self.secret_key = secret_key
         self.connector = connector
         self.__session: Optional[aiohttp.ClientSession] = None
 
@@ -57,6 +103,15 @@ class CoupangHTTPClient:
 
         method = route.method
         url = route.url
+
+        authorization = auth(method=method, path=url, access_key=self.access_key, secret_key=self.secret_key)
+
+        headers = {
+            "Authorization": authorization,
+            "Content-Type": "application/json"
+        }
+
+        kwargs["headers"] = headers
 
         async with self.__session.request(method, url, **kwargs) as r:
             data = await json_or_text(r)
