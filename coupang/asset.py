dcc1d9c7f814f00c96c11c44935c84982e2966c6
Hostagen <68284806+Hostagen@users.noreply.github.com>
2021-07-04 03:16:52 +0900

    add magic methods.

--- coupang/asset.py
+++ coupang/asset.py
@@ -23,6 +23,18 @@ class Asset(AssetMixin):
     def __init__(self, url):
         self._url = url
 
+    def __str__(self):
+        return self._url
+
+    def __len(self):
+        return len(self._url)
+
+    def __eq__(self, other):
+        return isinstance(other, Asset) and self._url == other._url
+
+    def __hash__(self):
+        return hash(self._url)
+
     @property
     def url(self):
         return self._url
