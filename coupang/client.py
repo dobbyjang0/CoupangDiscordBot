416e05ca0de371813f2f4d3cba8f1c1242505580
Hostagen <68284806+Hostagen@users.noreply.github.com>
2021-07-04 04:41:53 +0900

    nvm

--- coupang/client.py
+++ coupang/client.py
@@ -1,43 +1,4 @@
-import os
-import time
-import hmac
-import hashlib
-
 from . import http, product
-
-
-def _dt():
-    os.environ['TZ'] = 'GMT+0'
-    return time.strftime('%y%m%d') + 'T'+time.strftime('%H%M%S') + 'Z'
-
-
-def auth(
-        method: str,
-        access_key: str,
-        secret_key: str,
-        path: str,
-        query: str = None
-):
-
-    dt = _dt()
-
-    if query is not None:
-        message = f"{dt}{method}{path}{query}"
-
-    else:
-        message = f"{dt}{method}{path}"
-
-    signature = hmac.new(
-        bytes(secret_key, "utf-8"),
-        message.encode("utf-8"),
-        hashlib.sha256
-    ).hexdigest()
-
-    authorization = f"""
-    CEA algorithm=HmacSHA256, access-key={access_key}, signed-date={dt}, signature={signature}
-    """
-
-    return authorization.strip()
 
 
 class CoupangClient:
