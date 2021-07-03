6432e73a2284694dd54bf5bc0ffe19a10724aff1
Hostagen <68284806+Hostagen@users.noreply.github.com>
2021-07-04 03:25:53 +0900

    remove product from function names. add magic methods.

--- coupang/goldbox.py
+++ coupang/goldbox.py
@@ -7,17 +7,41 @@ class GoldBox:
     def __init__(self, data):
         self.data = data
 
+    def __str__(self):
+        return self.name
+
+    def __int__(self):
+        return self.id
+
+    def __eq__(self, other):
+        return isinstance(other, GoldBox) and self.id == other.id
+
     @property
-    def product_id(self) -> int:
+    def id(self) -> int:
         return self.data["productId"]
 
     @property
-    def product_image(self):
+    def name(self) -> str:
+        return self.data["productName"]
+
+    @property
+    def price(self) -> int:
+        return self.data["productPrice"]
+
+    @property
+    def url(self) -> str:
+        return self.data["productUrl"]
+
+    @property
+    def image(self):
         return Asset(self.data["productImage"])
 
     @property
-    def product_image_url(self):
-        return self.product_image.url
+    def mage_url(self):
+        return self.image.url
+
+    def is_free_shipping(self) -> bool:
+        return self.data["isFreeShipping"]
 
     def is_rocket(self) -> bool:
         return self.data["isRocket"]
