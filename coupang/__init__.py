48f4364df83ae066ab4e3bab959370b1642506d3
Hostagen <68284806+Hostagen@users.noreply.github.com>
2021-07-06 21:29:41 +0900

    fix import error.

--- coupang/__init__.py
+++ coupang/__init__.py
@@ -1,1 +1,6 @@
 __version__ = '0.1.0a'
+
+from .client import *
+from .http import *
+from . import product, asset
+
--- coupang/client.py
+++ coupang/client.py
@@ -1,5 +1,5 @@
-import asyncio
-from . import http, product
+from .http import CoupangHTTPClient
+from . import product
 
 
 class CoupangClient:
@@ -7,7 +7,7 @@ class CoupangClient:
     def __init__(self, access_key, secret_key):
         self.access_key = access_key
         self.secret_key = secret_key
-        self.http = http.CoupangHTTPClient(access_key=access_key, secret_key=secret_key)
+        self.http = CoupangHTTPClient(access_key=access_key, secret_key=secret_key)
 
     async def search(
             self,
