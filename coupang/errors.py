fae293237dd4cb260309454bf5043e3ffacfd690
Hostagen <68284806+Hostagen@users.noreply.github.com>
2021-07-08 18:28:25 +0900

    improve CoupangException error.

--- coupang/errors.py
+++ coupang/errors.py
@@ -1,13 +1,35 @@
+from aiohttp import ClientResponse
+from typing import Optional, Union, Dict, Any
+
+
 class Forbidden(Exception):
     pass
 
 
 class CoupangException(Exception):
-    pass
+
+    def __init__(self, response: ClientResponse, message: Optional[Union[str, Dict[str, Any]]]):
+        self.response: ClientResponse = response
+        self.status: int = response.status
+
+        self.code: int
+        self.text: str
+
+        if isinstance(message, dict):
+            self.code = message.get('rCode', 0)
+            self.text = message.get('rMessage', '')
+
+        else:
+            self.text = message or ''
+            self.code = 0
+
+        fmt = '{0.status} {0.reason} (error code: {1})'
+
+        if len(self.text):
+            fmt += ': {2}'
+
+        super().__init__(fmt.format(self.response, self.code, self.text))
 
 
 class InvalidSignature(CoupangException):
-
-    def __init__(self):
-        message = 'Invalid signature'
-        super().__init__(message)
+    pass
