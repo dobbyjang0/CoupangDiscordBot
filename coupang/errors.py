7b87a0fabee9563dd9acc360a2e11e28515801af
Hostagen <68284806+Hostagen@users.noreply.github.com>
2021-07-08 18:03:14 +0900

    add CoupangExceptions.

--- coupang/errors.py
+++ coupang/errors.py
@@ -1,2 +1,13 @@
 class Forbidden(Exception):
     pass
+
+
+class CoupangException(Exception):
+    pass
+
+
+class InvalidSignature(CoupangException):
+
+    def __init__(self):
+        message = 'Invalid signature'
+        super().__init__(message)
