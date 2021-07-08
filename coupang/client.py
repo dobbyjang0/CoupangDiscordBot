4669243d7f20953c5b71a8c3d97e337c80baba14
Hostagen <68284806+Hostagen@users.noreply.github.com>
2021-07-08 18:45:07 +0900

    event loop fix.

--- coupang/client.py
+++ coupang/client.py
@@ -1,13 +1,35 @@
+import asyncio
+
+from typing import Optional
 from .http import CoupangHTTPClient
 from . import product
 
 
 class CoupangClient:
 
-    def __init__(self, access_key, secret_key):
-        self.access_key = access_key
-        self.secret_key = secret_key
-        self.http = CoupangHTTPClient(access_key=access_key, secret_key=secret_key)
+    def __init__(
+            self,
+            *,
+            loop: Optional[asyncio.AbstractEventLoop] = None
+    ):
+
+        self.loop: asyncio.AbstractEventLoop = asyncio.get_event_loop() if loop is None else loop
+
+        self.http = CoupangHTTPClient(loop=self.loop)
+
+    def login(self, access_key: str, secret_key: str):
+
+        loop = self.loop
+
+        future = asyncio.ensure_future(
+            self.http.static_login(
+                access_key=access_key,
+                secret_key=secret_key
+            ),
+            loop=loop
+        )
+
+        loop.run_until_complete(future)
 
     async def search(
             self,
