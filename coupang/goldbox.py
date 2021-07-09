1968b6103451cb05dbb596716f238f658378960d
Hostagen <68284806+Hostagen@users.noreply.github.com>
2021-07-09 23:38:31 +0900

    add from_dict and to_dict.

--- coupang/goldbox.py
+++ coupang/goldbox.py
@@ -3,10 +3,6 @@ from .product import BaseProduct
 
 class GoldBox(BaseProduct):
 
-    def __init__(self, goldbox_payload):
-        self.rank = goldbox_payload["rank"]
-        super().__init__(goldbox_payload)
-
     def __str__(self):
         return self.name
 
@@ -15,11 +11,3 @@ class GoldBox(BaseProduct):
 
     def __eq__(self, other):
         return isinstance(other, GoldBox) and self.id == other.id
-
-    @property
-    def image(self):
-        return self._image
-
-    @property
-    def image_url(self):
-        return self.image.url
--- coupang/product.py
+++ coupang/product.py
@@ -12,7 +12,16 @@ class BaseProduct:
     def is_rocket(self) -> bool:
         return self._is_rocket
 
+    @property
+    def image(self):
+        return self._image
+
+    @property
+    def image_url(self):
+        return self.image.url
+
     def _update(self, data):
+        self.rank = data["rank"]
         self.name = data["productName"]
         self.id = int(data["productId"])
         self.price = int(data["productPrice"])
@@ -21,13 +30,24 @@ class BaseProduct:
         self._is_rocket = data["isRocket"]
         self._is_free_shipping = data["isFreeShipping"]
 
+    @classmethod
+    def from_dict(cls, data):
+        return cls(data)
+
+    def to_dict(self):
+        result = {
+            "rank": self.rank,
+            "isRocket": self.is_rocket(),
+            "isFreeShipping": self.is_free_shipping(),
+            "productId": self.id,
+            "productImage": self.image_url,
+            "productName": self.name,
+            "productPrice": self.price,
+            "productUrl": self.url
+        }
+
+        return result
+
 
 class Product(BaseProduct):
-
-    @property
-    def image(self):
-        return self._image
-
-    @property
-    def image_url(self):
-        return self.image.url
+    pass
