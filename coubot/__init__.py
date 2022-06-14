cc2003dc57674f70a34b8bfa7b689c3359326a7b
Hostagen <68284806+Hostagen@users.noreply.github.com>
2022-06-15 00:54:55 +0900

    update __init__.py

--- coubot/__init__.py
+++ coubot/__init__.py
@@ -4,8 +4,9 @@ __author__ = "dobbyjang"
 __version__ = "0.1.0a"
 
 from .db import *
-from .embed_form import *
+from .forms import *
 from .errors import *
 from .parser import *
 from .triggers import *
+from .colour import *
 from . import utils
